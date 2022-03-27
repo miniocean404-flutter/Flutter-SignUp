ab0d4563be0344832731cc7840ce552fd07245f9
MiniOcean404 <1037306928@qq.com>
2022-03-27 23:49:39 +0800

    视频播放底

--- lib/views/home/index.dart
+++ lib/views/home/index.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_sign_in/components/busin/qr_scanner.dart';
 import 'package:flutter_sign_in/components/common/modal.dart';
+import 'package:flutter_sign_in/utils/logger.dart';
+import 'package:video_player/video_player.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -12,6 +14,31 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   StateType _modalState = StateType.none;
+  late VideoPlayerController _controller;
+
+  @override
+  void initState() {
+    initVideo();
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _controller.dispose();
+  }
+
+  initVideo() async {
+    _controller = VideoPlayerController.network(
+      'https://flutter.github.io/assets-for-api-docs/assets/videos/bee.mp4',
+    );
+
+    await _controller.initialize();
+    setState(() {
+      _controller.setLooping(false);
+      _controller.play();
+    });
+  }
 
   closeModal() {
     setState(() {
@@ -29,39 +56,50 @@ class _HomeState extends State<Home> {
         child: StateModal(
           state: _modalState,
           onClose: closeModal,
-          bgChild: Column(
+          bgChild: Stack(
+            alignment: Alignment.center,
             children: [
-              SizedBox(height: 159.h),
-              const Text(
-                '签到',
-                style: TextStyle(fontSize: 29, fontWeight: FontWeight.w600),
-              ),
-              SizedBox(height: 102.h),
-              ClipRRect(
-                borderRadius: BorderRadius.circular(20),
-                child: SizedBox(
-                  width: 160.r,
-                  height: 160.r,
-                  child: const QRScanner(),
-                ),
-              ),
-              SizedBox(height: 158.h),
-              const Text(
-                '请向屏幕展示二维码',
-                style: TextStyle(color: Color(0xff999999), fontSize: 14.5),
-              ),
-              const Text(
-                '识别后会自动签到',
-                style: TextStyle(color: Color(0xff999999)),
-              ),
-              SizedBox(height: 292.h),
-              const Text(
-                'Serendipity Envoy',
-                style: TextStyle(
-                  color: Color(0xffCECECE),
-                  fontWeight: FontWeight.w400,
-                ),
-              ),
+              _controller.value.isInitialized
+                  ? AspectRatio(
+                      aspectRatio: 768 / 1024,
+                      child: VideoPlayer(_controller),
+                    )
+                  : const Text('空'),
+              Column(
+                children: [
+                  SizedBox(height: 159.h),
+                  const Text(
+                    '签到',
+                    style: TextStyle(fontSize: 29, fontWeight: FontWeight.w600),
+                  ),
+                  SizedBox(height: 102.h),
+                  ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: SizedBox(
+                      width: 160.r,
+                      height: 160.r,
+                      child: const QRScanner(),
+                    ),
+                  ),
+                  SizedBox(height: 158.h),
+                  const Text(
+                    '请向屏幕展示二维码',
+                    style: TextStyle(color: Color(0xff999999), fontSize: 14.5),
+                  ),
+                  const Text(
+                    '识别后会自动签到',
+                    style: TextStyle(color: Color(0xff999999)),
+                  ),
+                  SizedBox(height: 292.h),
+                  const Text(
+                    'Serendipity Envoy',
+                    style: TextStyle(
+                      color: Color(0xffCECECE),
+                      fontWeight: FontWeight.w400,
+                    ),
+                  ),
+                ],
+              )
             ],
           ),
         ),
