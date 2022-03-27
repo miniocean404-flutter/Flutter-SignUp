4ffc3f95ea4e52606e6b87a3fb3e120bbfe738b7
MiniOcean404 <1037306928@qq.com>
2022-03-27 17:01:13 +0800

    前置摄像头功能web实现，ios,android暂未调试

--- lib/config/global.dart
+++ lib/config/global.dart
@@ -15,7 +15,7 @@ class Global {
         maxWidth: MediaQuery.of(ctx).size.width,
         maxHeight: MediaQuery.of(ctx).size.height,
       ),
-      designSize: const Size(375, 812),
+      designSize: const Size(768, 1024),
       context: ctx, // 传入context会更灵敏的根据屏幕变化而改变
       minTextAdapt: true, // 是否根据宽度/高度中的最小值适配文字
       orientation: Orientation.portrait, // 屏幕方向
--- lib/views/home/index.dart
+++ lib/views/home/index.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 
 class Home extends StatefulWidget {
   const Home({Key? key}) : super(key: key);
@@ -18,21 +19,36 @@ class _HomeState extends State<Home> {
       body: Center(
         child: Column(
           children: [
-            SizedBox(height: 100.h),
+            SizedBox(height: 159.h),
             const Text(
               '签到',
               style: TextStyle(fontSize: 29, fontWeight: FontWeight.w600),
             ),
-            SizedBox(height: 100.h),
-            Container(
-              width: 100.r,
-              height: 100.r,
-              decoration: const BoxDecoration(
-                color: Colors.red,
-                borderRadius: BorderRadius.all(Radius.circular(10)),
+            SizedBox(height: 102.h),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: SizedBox(
+                width: 160.r,
+                height: 160.r,
+                child: Container(
+                    // child: MobileScanner(
+                    //   allowDuplicates: false, // 扫描一次还是(true)多次
+                    //   fit: BoxFit.cover,
+                    //   controller: MobileScannerController(
+                    //     // 相机朝向
+                    //     facing: CameraFacing.front,
+                    //     torchEnabled: true,
+                    //   ),
+                    //   // 发现二维码事件
+                    //   onDetect: (barcode, args) {
+                    //     final String? code = barcode.rawValue;
+                    //     debugPrint('二维码 $code');
+                    //   },
+                    // ),
+                    ),
               ),
             ),
-            SizedBox(height: 100.h),
+            SizedBox(height: 158.h),
             const Text(
               '请向屏幕展示二维码',
               style: TextStyle(color: Color(0xff999999), fontSize: 14.5),
@@ -41,7 +57,7 @@ class _HomeState extends State<Home> {
               '识别后会自动签到',
               style: TextStyle(color: Color(0xff999999)),
             ),
-            SizedBox(height: 200.h),
+            SizedBox(height: 292.h),
             const Text(
               'Serendipity Envoy',
               style: TextStyle(
