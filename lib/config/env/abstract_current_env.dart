20bf7d7ecca87f5a38fad314c86e268e41ecca10
戴海洋 <haiyang.dai@davincimotor.com>
2022-05-11 12:00:54 +0800

    fix: 初始化请求地址问题

--- lib/config/env/abstract_current_env.dart
+++ lib/config/env/abstract_current_env.dart
@@ -1,5 +1,5 @@
 abstract class CurrentEnv {
-  late String baseUrl;
+  late String baseUrl = '';
 
   // set get 只供外部使用,其他类及自身无法调用
   String get getBaseUrl => baseUrl;
--- lib/config/env/env.dev.dart
+++ lib/config/env/env.dev.dart
@@ -3,7 +3,17 @@ import 'package:flutter_sign_in/utils/shared_preferences.dart';
 
 class EnvironmentDev extends CurrentEnv {
   EnvironmentDev() {
+    final getUrl = SpHelper.getLocalStorage('baseUrl');
     baseUrl = 'http://10.30.13.134:4040';
-    SpHelper.setLocalStorage('baseUrl', baseUrl);
+
+    RegExp reg = RegExp(r"\/\/([\d\w.]+)", multiLine: true, unicode: true);
+    final getURL = reg.firstMatch(getUrl)?.group(1);
+    final baseURL = reg.firstMatch(baseUrl)?.group(1);
+
+    if (getURL != baseURL) {
+      SpHelper.setLocalStorage('baseUrl', baseUrl);
+    } else {
+      baseUrl = getUrl;
+    }
   }
 }
