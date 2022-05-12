56946073ec1ea704b1b4b5f7c66a399e807d0f0b
MiniOcean404 <1037306928@qq.com>
2022-05-12 21:35:03 +0800

    chore: 添加域名环境

--- lib/config/env/env.dev.dart
+++ lib/config/env/env.dev.dart
@@ -4,7 +4,8 @@ import 'package:flutter_sign_in/utils/shared_preferences.dart';
 class EnvironmentDev extends CurrentEnv {
   EnvironmentDev() {
     final getUrl = SpHelper.getLocalStorage('baseUrl');
-    baseUrl = 'http://10.30.13.134:4040';
+    baseUrl = 'http://api.uat.serendipity.illiaccess.com';
+    // baseUrl = 'http://10.30.13.134:4040';
     // baseUrl = 'http://192.168.1.130:4040';
 
     RegExp reg = RegExp(r"\/\/([\d\w.]+)", multiLine: true, unicode: true);
